9f8523071e1f34a10de972e4a0b8512b0136e78c
elvisaguilar704 <63757562+elvisaguilar704@users.noreply.github.com>
2021-10-04 10:57:08 -0400

    tetris.4
    rotate key added

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -71,6 +71,12 @@ document.addEventListener('DOMContentLoaded', () => {
       function control(e) {
           if(e.keyCode === 37) {
               moveLeft()
+          } else if (e.keyCode === 38) {
+              rotate()
+          } else if (e.keyCode === 39) {
+              moveRight()
+          } else if (e.keyCode === 40) {
+              moveDown()
           }
       }
       document.addEventListener('keyup', control)
@@ -108,6 +114,37 @@ document.addEventListener('DOMContentLoaded', () => {
 
           draw()  
       }
+
+      //move the tetromino right, unless is at the edge or there is a blockage
+      function moveRight() {
+          undraw()
+          const isAtRightEdge = current.some(index => (currentPosition + index) % width === width -1)
+
+          if(!isAtRightEdge) currentPosition +=1
+
+          if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+              currentPosition -=1
+          }
+
+          draw()
+        }
+
+        //rotate the tetromino
+        function rotate() {
+            undraw()
+            currentRotation ++
+            if(currentRotation === current.length) { //if current rotation gets to 4, make it go back to 0
+                currentRotation = 0
+            }
+            current = theTetrominoes[random][currentRotation]
+            draw()
+        }
+
+
+
+
+
+
 
 
 

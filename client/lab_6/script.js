f34a39c16d2a5e1246250bedac072ab40678c797
elvisaguilar704 <63757562+elvisaguilar704@users.noreply.github.com>
2021-10-07 22:50:17 -0400

    lab.6.pt3
    semicolons added

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -3,44 +3,41 @@ const suggestions = document.querySelector('.suggestions');
 
 async function windowActions() {
   const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
-  
 
-  const request = await fetch(endpoint)
+  const request = await fetch(endpoint);
 
   const arrayName = await request.json();
-    
 
-   // .then((blob) => blob.json())
-   // .then((data) => arrayName.push(...data));
+  // .then((blob) => blob.json())
+  // .then((data) => arrayName.push(...data));
 
   function findMatches(wordToMatch, arrayName) {
     return arrayName.filter((place) => {
-        //console.log(place)
+      // console.log(place)
       // here we need to figure out if city and state matches what was searched
       const regex = new RegExp(wordToMatch, 'gi');
-      //return place.city.match(regex) || place.state.match(regex);
+      // return place.city.match(regex) || place.state.match(regex);
       return place.name.match(regex) || place.category.match(regex);
     });
   }
 
- // function numberWithCommas(x) {
+  // function numberWithCommas(x) {
   //  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');}
 
-  
   function displayMatches(event) {
     const matchArray = findMatches(event.target.value, arrayName);
-    //console.log(matchArray)
+    // console.log(matchArray)
     const html = matchArray.map((place) => {
       const regex = new RegExp(event.target.value, 'gi');
-      const cityName = place.city   
-      const placeName = place.name
-      const categoryName = place.category
-      const addressName = place.address_line_1
-      const zipName = place.zip
-      //.replace(regex, `<span class="h1">${event.target.value}</span>`);
-      console.log(cityName) 
-      const stateName = place.state  
-      //.replace(regex, `<span class="h1">${event.target.value}</span>`);
+      const cityName = place.city;
+      const placeName = place.name;
+      const categoryName = place.category;
+      const addressName = place.address_line_1;
+      const zipName = place.zip;
+      // .replace(regex, `<span class="h1">${event.target.value}</span>`);
+      console.log(cityName);
+      const stateName = place.state;
+      // .replace(regex, `<span class="h1">${event.target.value}</span>`);
       return `
                 <li>
                     <span class="name">${placeName}</span> </br>
@@ -56,8 +53,8 @@ async function windowActions() {
     suggestions.innerHTML = html;
   }
 
-  //const searchInput = document.querySelector('.search');
-  //const suggestions = document.querySelector('.suggestions');
+  // const searchInput = document.querySelector('.search');
+  // const suggestions = document.querySelector('.suggestions');
 
   searchInput.addEventListener('change', displayMatches);
 
